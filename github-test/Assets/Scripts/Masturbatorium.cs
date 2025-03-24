a76ad926620f8b81d27302b33fcf813048239af0
alexanderajfraser <alexander.aj.fraser@gmail.com>
2025-03-24 17:32:08 -0400

    Shits and giggles.

---
+++ github-test/Assets/Scripts/Masturbatorium.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Masturbatorium : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("Hi Class!");
+    }
+
+}
